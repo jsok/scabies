f9f93e63621348e5e9f534037873d45ac2dd64af
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2011-03-25 23:30:38 +1100

    Add a name column to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ require 'digest/sha1'
 
 class User < ActiveRecord::Base
   validates_length_of :password, :within => 5..40
-  validates_presence_of :email, :password, :password_confirmation, :salt
+  validates_presence_of :email, :name, :password, :password_confirmation, :salt
   validates_uniqueness_of :email
   validates_confirmation_of :password
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"  
